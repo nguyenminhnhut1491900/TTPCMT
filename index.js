dba2171b74837db22669c879f4b2fab5f7153cc9
Nguyễn Minh Nhựt <nguyenminhnhut1491900@gmail.com>
2018-05-15 16:03:13 +0700

    Sua lai bien PORT

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-								//Đặt địa chỉ Port được mở ra để tạo ra chương trình mạng Socket Server
-
 var http = require('http');
 var express = require('express');							//#include thư viện express - dùng để tạo server http nhanh hơn thư viện http cũ
 var socketio = require('socket.io')				//#include thư viện socketio
@@ -17,7 +15,9 @@ var middleware = require('socketio-wildcard')();		//Để có thể bắt toàn 
 esp8266_nsp.use(middleware);									//Khi esp8266 emit bất kỳ lệnh gì lên thì sẽ bị bắt
 webapp_nsp.use(middleware);									//Khi webapp emit bất kỳ lệnh gì lên thì sẽ bị bắt
 
-server.listen(process.env.PORT || 3000);										// Cho socket server (chương trình mạng) lắng nghe ở port 3484
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT);										// Cho socket server (chương trình mạng) lắng nghe ở port 3484
 console.log("Server nodejs chay tai dia chi: " + ip.address() + ":" + PORT)
 
 //Cài đặt webapp các fie dữ liệu tĩnh
